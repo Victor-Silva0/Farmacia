a19123d5731bbd4a1859f3351e35314766d629cc
Victor Amaral Silva <victoramaral.silva@hotmail.com>
2023-10-22 17:23:49 -0300

    Base

---
+++ Farmacia/app/Views/layouts/footer.php
@@ -0,0 +1,13 @@
+<p> </p>
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">Copyright  © FATEC Presidente Prudente <?= date('Y') ?>.</p>
+        </div>
+    </footer>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/jquery-3.2.1.min.js"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/jquery.validate.min.js"type="text/javascript"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/bootstrap.min.js"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/holder.min.js"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/localfilereader.js"></script>
+</body>
+</html>
