ac5fed626dd77a5d51635c67e506ea5fe7d1fb51
Victor Amaral Silva <victoramaral.silva@hotmail.com>
2023-10-28 12:06:31 -0300

    Add files via upload

---
+++ farmacia/App/Models/Entidades/Produto.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models\Entidades;
+
+
+class Produto
+{
+    private $id;
+    private $nome;
+    private $marca;
+    private $conteudo;
+    private $preco;
+    private $imagem;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+    }
+
+    public function getConteudo()
+    {
+        return $this->conteudo;
+    }
+
+    public function setConteudo($conteudo)
+    {
+        $this->conteudo = $conteudo;
+    }
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+    }
+
+    public function getImagem()
+    {
+        return $this->imagem;
+    }
+
+    public function setImagem($imagem)
+    {
+        $this->imagem = $imagem;
+    }
+}
