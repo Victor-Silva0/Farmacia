086a3d1887796e43b30a72223dfd2dd7e78535ef
Breno <71463305+brenopremoli@users.noreply.github.com>
2023-10-29 23:32:06 -0300

    Add files via upload

--- farmacia/App/Views/layouts/footer.php
+++ farmacia/App/Views/layouts/footer.php
@@ -6,5 +6,10 @@
     <p>&nbsp;</p>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+<script src="http://<?php echo APP_HOST; ?>/public/js/jquery-3.2.1.min.js"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/jquery.validate.min.js"type="text/javascript"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/bootstrap.min.js"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/holder.min.js"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/localfilereader.js"></script>
 </body>
 </html>
--- farmacia/App/Views/layouts/header.php
+++ farmacia/App/Views/layouts/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PharmaX</title>
-    <link rel="shortcut icon" href="public/images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="public/css/style.css">
+    <title>FarmaTech</title>
+    <link rel="shortcut icon" href="http://<?php echo APP_HOST; ?>/public/images/favicon.ico" type="image/x-icon">
+    <link href="http://<?php echo APP_HOST; ?>/public/css/style.css" rel="stylesheet">
     <style>
       header { font-family: 'Montserrat', sans-serif;
       }
@@ -16,5 +16,6 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
+  
+  </head>
 <body>
