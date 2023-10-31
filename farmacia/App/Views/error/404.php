bae8c0f58515eb84f6f5f27952d84283c7764131
Breno <71463305+brenopremoli@users.noreply.github.com>
2023-10-30 21:09:20 -0300

    Add files via upload

---
+++ farmacia/App/Views/error/404.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h2 class="error"><?= TITLE ?></h2>
+    <h1 class="error"><?= "Erro 404: {$varMessage}" ?></h1>
+</div>
--- farmacia/App/Views/produto/index.php
+++ farmacia/App/Views/produto/index.php
@@ -13,7 +13,7 @@
                         <span class="input-group-addon input-sm" id="basic-addon1">
                             <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
                         </span>
-                        <input type="text" placeholder="Buscar conteúdo" value="" class="form-control input-sm" name="busca" />
+                        <input type="text" placeholder="Digite..." value="" class="form-control input-sm" name="busca" />
 
                         <div class="input-group-btn">
                             <button class="btn btn-success btn-sm" type="submit">Buscar</button>
@@ -33,7 +33,7 @@
 
                 <?php if($Sessao::retornaMensagem()){ ?>
                 <div class="alert alert-warning" role="alert">
-                    <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    <a href="" class="close link-secondary" data-dismiss="alert" aria-label="close"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></a>
                     <?= $Sessao::retornaMensagem() ?> 
                     <br>
                 </div>
