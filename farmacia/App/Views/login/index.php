e2e595d5712915300fa8dcd5acc4f27506ea09d7
Breno <71463305+brenopremoli@users.noreply.github.com>
2023-11-03 19:58:18 -0300

    Add files via upload

--- farmacia/App/Views/login/index.php
+++ farmacia/App/Views/login/index.php
@@ -12,12 +12,7 @@
 
             <?php if ($Sessao::retornaMensagem()) { ?>
                 <div class="alert alert-warning" role="alert">
-                    <a href="" class="close link-secondary" data-dismiss="alert" aria-label="close">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                        </svg>
-                    </a>
+                <a href="" class="close link-secondary" data-dismiss="alert" aria-label="close"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></a>
                     <?= $Sessao::retornaMensagem() ?>
                     <br>
                 </div>
@@ -25,12 +20,7 @@
 
             <?php if ($Sessao::retornaErro()) { ?>
                 <div class="alert alert-warning" role="alert">
-                    <a href="" class a="close link-secondary" data-dismiss="alert" aria-label="close">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                        </svg>
-                    </a>
+                <a href="" class="close link-secondary" data-dismiss="alert" aria-label="close"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></a>
                     <?php foreach ($Sessao::retornaErro() as $key => $mensagem) {
                         echo $mensagem . "<br />";
                     } ?>
@@ -40,12 +30,12 @@
             <div class="table-responsive">
                 <form action="http://<?php echo APP_HOST; ?>/login/autentica" method="post" id="form_cadastro">
                     <div class="form-group">
-                        <label for="nomeusuario">Usuário:</label>
-                        <input type="text" class="form-control" name="nomeusuario" value="" required>
+                        <label for="username">Usuário:</label>
+                        <input type="text" class="form-control" name="username" value="" required>
                     </div>
                     <div class="form-group">
                         <label for="password">Senha:</label>
-                        <input type="password" class="form-control" name="senha" value="" required>
+                        <input type="password" class="form-control" name="password" value="" required>
                     </div>
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary btn-sm">
