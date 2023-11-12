ca221327eebea0ad7fdde4ce275c1db812a7d8c9
Rafael Fernandes <91971591+RafaelFernandes1992@users.noreply.github.com>
2023-11-12 17:08:48 -0300

    Update ProdutoVendaController.php

--- farmacia/App/Controllers/ProdutoVendaController.php
+++ farmacia/App/Controllers/ProdutoVendaController.php
@@ -4,53 +4,26 @@ namespace App\Controllers;
 
 use App\Lib\Sessao;
 use App\Models\DAO\ProdutosVendaDAO;
+use App\Models\Entidades\Vendas;
 
 
 class ProdutoVendaController extends Controller
 {
     public function index()
     {
-        if (!$this->auth()) $this->redirect('/login');
+        if (!$this->auth()) $this->redirect('/login/index');
 
         $produtosVendaDAO = new ProdutosVendaDAO();
 
-
+        $listaprodutosVenda = $produtosVendaDAO->listar();
+        
+        self::setViewParam('listaProdutoVenda', $listaprodutosVenda);
 
         $this->render('/produto_venda/index');
 
+
         Sessao::limpaErro();
         Sessao::limpaMensagem();
-
     }
 
-    public function cadastro()
-    {
-
-    }
-
-    public function salvar()
-    {
-
-    }
-
-    public function edicao($params)
-    {
-
-    }
-
-    public function atualizar()
-    {
-
-    }
-
-    public function exclusao($params)
-    {
-
-    }
-
-    public function excluir()
-    {
-
-    }
 }
-
