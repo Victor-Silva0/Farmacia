cb32ab14f3df2b1864e12b8a4023603042309bb8
Rafael Fernandes <91971591+RafaelFernandes1992@users.noreply.github.com>
2023-11-14 23:56:24 -0300

    Update ProdutoVendaController.php

--- farmacia/App/Controllers/ProdutoVendaController.php
+++ farmacia/App/Controllers/ProdutoVendaController.php
@@ -4,8 +4,7 @@ namespace App\Controllers;
 
 use App\Lib\Sessao;
 use App\Models\DAO\ProdutosVendaDAO;
-use App\Models\Entidades\Vendas;
-
+use App\Models\Entidades\ProdutosVenda;
 
 class ProdutoVendaController extends Controller
 {
@@ -24,6 +23,53 @@ class ProdutoVendaController extends Controller
 
         Sessao::limpaErro();
         Sessao::limpaMensagem();
+    }
+
+    public function cadastro()
+    {
+        $this->render('/produto_venda/cadastro');
+
+        Sessao::limpaFormulario();
+        Sessao::limpaMensagem();
+        Sessao::limpaErro();
+    }
+
+    public function salvar()
+    {
+        $produto = new ProdutosVenda();
+        $produto->setProduto($_POST['idproduto']);
+        $produto->setVendas($_POST['idvenda']);
+        $produto->setQuantidade($_POST['quantidade']);
+
+
+        Sessao::gravaFormulario($_POST);
+
+        //$produtoValidador = new ProdutoValidador();
+        //$resultadoValidacao = $produtoValidador->validar($produto);
+
+        //if ($resultadoValidacao->getErros()) {
+        //    Sessao::gravaErro($resultadoValidacao->getErros());
+        //    $this->redirect('/produto/cadastro');
+        //}
+
+        try {
+
+            $produtosVendaDAO = new ProdutosVendaDAO(); 
+            $lastId = $produtosVendaDAO->salvar($produto);
+            $produto->setId($lastId);
+
+        } catch (\Exception $e) {
+            Sessao::gravaMensagem($e->getMessage());
+            $this->redirect('/produto_venda');
+        }
+
+        Sessao::limpaFormulario();
+        Sessao::limpaMensagem();
+        Sessao::limpaErro();
+
+        Sessao::gravaMensagem("Produto adicionado com sucesso!");
+        
+        $this->redirect('/produto');
     }
 
 }
